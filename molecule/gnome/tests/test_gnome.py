28e100afdf2fe71e10fba8bdae8a33e396a66988
John Freeman <john.freeman.opensource@gmail.com>
2018-08-22 13:16:04 +0000

    Added configuration for dock position
    For those who prefer a different dock position.

--- molecule/gnome/tests/test_gnome.py
+++ molecule/gnome/tests/test_gnome.py
@@ -24,7 +24,8 @@ def test_for_config_dirs(host, dir_path):
 @pytest.mark.parametrize('config_path', [
     '/usr/share/X11/xorg.conf.d/60-ansible-screensaver.conf',
     '/usr/share/glib-2.0/schemas/20_ansible_screensaver.gschema.override',
-    '/usr/share/glib-2.0/schemas/20_ansible_lockscreen.gschema.override'
+    '/usr/share/glib-2.0/schemas/20_ansible_lockscreen.gschema.override',
+    '/usr/share/glib-2.0/schemas/20_ansible_dock.gschema.override'
 ])
 def test_for_config(host, config_path):
     config_file = host.file(config_path)
